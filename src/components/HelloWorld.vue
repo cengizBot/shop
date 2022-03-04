95598f8b8a314427fdb483860d516f0ff0a53984
cengizBot <kurtt.cengiz@gmail.com>
2022-03-04 16:34:08 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <input type="file" @change="onFileChange" />
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters([
+      'products'
+    ]),
+  },
+  watch : {
+    products () {
+      console.log('products change', this.products.length);
+    }
+  },
+  mounted() {
+    console.log('products',this.products.length);
+
+  },
+  methods : {
+    onFileChange(e) {
+
+      const files = e.currentTarget.files;
+
+      if (files.length > 0)
+      {
+        const fileToLoad = files[0];
+        const fileReader = new FileReader();
+
+        fileReader.onload = function(fileLoadedEvent)
+        {
+         console.log('fileLoadedEvent.target.result',fileLoadedEvent.target.result);
+        };
+
+        fileReader.readAsDataURL(fileToLoad);
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
